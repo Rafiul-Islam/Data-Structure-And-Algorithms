4441cf52b54e492d7136eda2a9376acd3e28137b
Rafiul-Islam <rafi.cse.ru@gmail.com>
2020-11-09 13:32:56 +0600

    Liner search code updated

--- Algorithms/Searching/LinearSearch.py
+++ Algorithms/Searching/LinearSearch.py
@@ -1,12 +1,11 @@
 def linear_search(array, target):
     for index, value in enumerate(array):
         if value == target:
-            print(f"{target} exist in the list and the index is: {index}")
-            return
+            return index
     else:
-        print(f"{target} does not exist in the following list")
+        return -1
 
 
 li = [1, 2, 3, 4, 5, 6, 7]
-target = 5
-linear_search(li, target)
+target = 10
+print("Element found at index", linear_search(li, target))
